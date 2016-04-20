f28bb1b953ecc8f441332ba837235279ad581607
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-04-20 16:21:43 -0400

    Test /api/v1 response content

--- test/integration/controllers/v1/ApiController.test.js
+++ test/integration/controllers/v1/ApiController.test.js
@@ -32,7 +32,6 @@ describe('Requests to the root (/api) path', function() {
         done();
       });
   });
-
 });
 
 /**
@@ -52,5 +51,21 @@ describe('Requests to v1 root (/api/v1) path', function() {
     request(sails.hooks.http.app)
       .get('/api/v1')
       .expect("content-type", /json/, done)
+  });
+
+  it('GET: Returns v1 route', function(done) {
+
+    request(sails.hooks.http.app)
+      .get('/api/v1')
+      .expect("content-type", /json/)
+      .end(function(err, res) {
+        if (err) throw err;
+        res.body.should.have.property('user');
+        res.body.user.should.have.property('register');
+        res.body.user.register.should.endWith('/api/v1/user/register');
+        res.body.user.should.have.property('password');
+        res.body.user.password.should.endWith('/api/v1/user/password');
+        done();
+      });
   });
 });
