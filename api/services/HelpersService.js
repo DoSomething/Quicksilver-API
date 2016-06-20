0b1cbcaf6d2f2dc6f5c896926b072b54da3c85e3
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-06-20 16:59:07 -0400

    Cleanout NorthstarService logic - should have not added to repo first

--- api/services/HelpersService.js
+++ api/services/HelpersService.js
@@ -1,44 +1,9 @@
 'use strict';
 
-const NorthstarClient = require('@dosomething/northstar-js');
-
 /**
- * Nortstar client service.
+ * Helpers services. A collection of services to be used throughout the application.
  */
 module.exports = {
-
-  getClient() {
-    if (!this.client) {
-      this.client = new NorthstarClient({
-        baseURI: sails.config.northstar.apiBaseURI,
-        apiKey: sails.config.northstar.apiKey,
-      });
-    }
-    return this.client;
-  },
-
-  getUserFor(model) {
-    const options = this.resolveRequestId(model);
-    return this.getClient().getUser(options.type, options.id);
-  },
-
-  resolveRequestId(model) {
-    let id;
-    let type;
-
-    if (model.user_id) {
-      type = 'id';
-      id = model.user_id;
-    } else if (model.email) {
-      type = 'email';
-      id = model.email;
-    } else if (model.mobile) {
-      type = 'mobile';
-      id = model.mobile;
-    }
-
-    return { type, id };
-  },
 
   isOneOfFieldSet(submission) {
     return ['user_id', 'email', 'mobile'].every(userField => !submission[userField]);
